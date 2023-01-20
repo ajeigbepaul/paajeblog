a335267d9b67a6b4d99cd216f17cd44c7ae0d52b
ajeigbe paul <ajeigbepaul@yahoo.com>
2023-01-20 20:27:08 +0100

    blog

---
+++ app/(user)/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function page() {
+  return (
+    <div className="text-4xl text-yellow-400">Welcome to my Blog</div>
+  )
+}
+
+export default page
---
+++ sanity.config.ts
@@ -0,0 +1,28 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+import { myTheme } from './theme'
+import StudioNavbar from './components/StudioNavbar'
+
+export default defineConfig({
+  basePath:"/studio",
+  name: 'PAAJE_content_studio',
+  title: 'PAAJE Content studio',
+
+  projectId:"1j6yn8b7",
+  dataset:"production",
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+  studio:{
+    components:{
+      // logo:Logo,
+      navbar:StudioNavbar
+    }
+  },
+  theme: myTheme
+})
