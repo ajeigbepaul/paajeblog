fe80377ec67d17945585378a0bab6a4532ba3ed1
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-01-29 21:41:22 -0800

    finish the blog site

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,18 +5,18 @@ function Banner() {
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
         <div>
             <h1 className="text-7xl">
-                Fresh and fit blog
+                Be-Natural Blog
             </h1>
-            <h2 className="mt-5 md:mt-0">
+            <h2 className="mt-5 md:mt-0 text-1xl">
                 welcome to {" "}
                 <span className="underline decoration-4 decoration-orange-300">
-                Every ladies blog
+                Be-Natural 
                 </span>{" "}
-                about healthy and Natural products
+                all for your body and skin
             </h2>
 
         </div>
-        <h2 className="mt-5 max-w-sm md:mt-2 text-gray-500 ">New product features| Natural Skin blog| Healthy living</h2>
+        <h2 className="mt-5 max-w-sm md:mt-2 text-gray-500 font-sans ">New product features| Natural Skin blog| Healthy living</h2>
     </div>
   )
 }
